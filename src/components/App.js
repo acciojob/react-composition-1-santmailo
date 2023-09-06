6601f1917d93b8e2de879a410f7d938443697cfc
santmailo <iamsunil006@gmail.com>
2023-09-07 00:13:41 +0530

    done adding tabs component

--- src/components/App.js
+++ src/components/App.js
@@ -4,46 +4,55 @@ import './../styles/App.css';
 
 
 const Tabs = ({ tabs }) => {
-    const [content, setContent] = useState(tabs[0].content);
+  const [content,setContent] = useState(tabs[0].content);
+
+
+  const clickMe = (tab) => {
+    setContent(tab.content);
+  }
+
 
     return (
       <div className="tabs">
-        <ul className="tab-list">
-            {tabs.map((tab, index) => {
-            return (
-                <li key={index} className="tab-list-item" onClick={() => {
-                    setContent(tab.content);
-                }}>
-                {tab.title}
-                </li>
-            )
-            })}
+        <ul>
+          {
+            tabs.map((tab) => {
+              return <Tab key={tab.title} tab={tab} clickMe={()=>{clickMe(tab)}}/>
+            })
+          }
         </ul>
-        <div className="tab-content">
-            {content}
-        </div>
+        {content}
       </div>
     )
+}
+
+const Tab = ({tab, clickMe}) => {
+
+  return (
+    <li onClick={clickMe}>{tab.title}</li>
+  )
 }
 
 const App = () => {
   const tabs = [
     {
       title: "Tab 1",
-      content: "This is the content for Tab 1"
+      content: 'This is the content for Tab 1.'
     },
     {
       title: "Tab 2",
-      content: "This is the content for Tab 2"
+      content: 'This is the content for Tab 2.'
     },
     {
       title: "Tab 3",
-      content: "This is the content for Tab 3"
+      content: 'This is the content for Tab 3.'
     }
   ]
+
   return (
-    <Tabs tabs={tabs}/>
+    <Tabs tabs={tabs} />
   )
+
 }
 
 export default App
