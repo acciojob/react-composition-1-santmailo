aabb1a1dd6db001ff0ed2d17209acbdfaea4bcf0
santmailo <iamsunil006@gmail.com>
2023-09-06 23:54:21 +0530

    hey

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,30 @@
 
-import React from "react";
-import Tabs from "./Tabs";
+import React,{useState} from "react";
 import './../styles/App.css';
+
+
+const Tabs = ({ tabs }) => {
+    const [content, setContent] = useState(tabs[0].content);
+
+    return (
+      <div className="tabs">
+        <ul className="tab-list">
+            {tabs.map((tab, index) => {
+            return (
+                <li key={index} className="tab-list-item" onClick={() => {
+                    setContent(tab.content);
+                }}>
+                {tab.title}
+                </li>
+            )
+            })}
+        </ul>
+        <div className="tab-content">
+            {content}
+        </div>
+      </div>
+    )
+}
 
 const App = () => {
   const tabs = [
